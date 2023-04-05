31d540c3e7959fd107d37cfd5398cbeba92d63a5
Sam Matenaer <smatenaer@esri.com>
2023-04-05 17:35:04 -0500

    update tests

--- tests/ArcView.test.tsx
+++ tests/ArcView.test.tsx
@@ -1,41 +1,30 @@
-import { cleanup, render, screen } from '@testing-library/react';
+import WebMap from '@arcgis/core/WebMap';
+import { cleanup, render } from '@testing-library/react';
+import { StrictMode } from 'react';
 import { describe, expect, it, vitest } from 'vitest';
 
 import { ArcMapView, ArcSceneView } from '../src';
 
 // lol the arcgis api doesn't seem to have any good testing resources...
-vitest.mock('@arcgis/core/views/MapView', async () => ({
-  default: class MapView {
-    constructor() {}
-
-    when() {
-      return Promise.resolve();
-    }
-  },
-  // your mocked methods
-}));
-vitest.mock('@arcgis/core/views/SceneView', async () => ({
-  default: class SceneView {
-    constructor() {}
-    when() {
-      return Promise.resolve();
-    }
-  },
-
-  // your mocked methods
-}));
-vitest.mock('@arcgis/core/WebMap', () => ({
-  default: class WebMap {
-    constructor() {
-      return {};
-    }
-  },
-}));
+vitest.mock(
+  '@arcgis/core/views/MapView',
+  async () => import('@arcgis/core/views/View')
+);
+vitest.mock(
+  '@arcgis/core/views/SceneView',
+  async () => import('@arcgis/core/views/View')
+);
 
 describe('ArcView', () => {
   it('renders sceneview', () => {
-    const { container } = render(<ArcSceneView />);
+    const map = new WebMap({ basemap: 'streets-vector' });
+    const { container } = render(
+      <StrictMode>
+        <ArcSceneView map={map} style={{ height: '100vh' }} />
+      </StrictMode>
+    );
     expect(container).toMatchSnapshot();
+
     cleanup();
     expect(container).toMatchSnapshot();
   });
