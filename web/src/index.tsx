93143399c1c8b32fa08edca203a3a429bc8d4883
Sam Matenaer <smatenaer@esri.com>
2023-07-10 16:31:50 -0500

    restructure project

---
+++ web/src/index.tsx
@@ -0,0 +1,29 @@
+import '@arcgis/core/assets/esri/themes/dark/main.css';
+import './index.css';
+
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+
+import { App } from './App';
+
+// Create a root element for the application
+const root = createRoot(document.querySelector('#root') as HTMLElement);
+
+const queryClient = new QueryClient({
+  defaultOptions: { queries: { refetchOnWindowFocus: false } },
+});
+
+import { defineCustomElements } from '@esri/calcite-components/dist/loader';
+
+// CDN hosted assets
+defineCustomElements(window);
+
+// Render the application
+root.render(
+  <StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
+  </StrictMode>
+);
