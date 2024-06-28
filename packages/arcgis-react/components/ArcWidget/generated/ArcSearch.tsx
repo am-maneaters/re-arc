ef6b684ea55f39da0bc26edd98ec460db6fd4940
Jonny Dawe <59748759+JonnyDawe@users.noreply.github.com>
2024-06-28 19:57:56 +0100

    Update ArcSearch typings to use correct properties (#19)
    Co-authored-by: Jonny Dawe <jonwe@bas.ac.uk>

--- packages/arcgis-react/components/ArcWidget/generated/ArcSearch.tsx
+++ packages/arcgis-react/components/ArcWidget/generated/ArcSearch.tsx
@@ -2,8 +2,7 @@ import Search from '@arcgis/core/widgets/Search';
 
 import { createWidget } from '../../util/createWidget';
 export const ArcSearch = createWidget<
-  // @ts-expect-error - Search is not typed correctly
   typeof Search,
-  __esri.SearchProperties,
+  __esri.widgetsSearchProperties,
   Search
 >(Search);
